15a0228f15e89fa9f054656a54bd723ee99a6be2
Omor Sifat <omorsefat0221@gmail.com>
2024-11-08 18:42:48 +0600

    JavaScript done

---
+++ 07-JavaScript/01-JS/index.js
@@ -0,0 +1,16 @@
+// Operator 
+var x=10;
+var y=20;
+var sum=x+y;
+var sub=x-y;
+var mul=x*y;
+var div=x/y;
+var mod=x%y;
+
+console.log(sum)
+console.log(sub)
+console.log(mul)
+console.log(div)
+console.log(mod)
+
+var mod
